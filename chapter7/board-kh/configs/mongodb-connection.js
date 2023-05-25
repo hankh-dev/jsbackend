84c607e323f7448d2533498fe2601b242ef38081
hankh <hankh.dev@gmail.com>
2023-05-25 22:24:05 +0900

    feat: query logging

--- chapter7/board-kh/configs/mongodb-connection.js
+++ chapter7/board-kh/configs/mongodb-connection.js
@@ -3,5 +3,9 @@ const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://hankh:${process.env.DB_PASS}@cluster0.8walzr5.mongodb.net/board?retryWrites=true&w=majority`;
 
 module.exports = function (callback) {
-  return MongoClient.connect(uri, callback);
+  const client = new MongoClient(uri, { monitorCommands:true });
+  client.on('commandStarted', (event) => console.debug(event));
+  client.on('commandSucceeded', (event) => console.debug(event));
+  client.on('commandFailed', (event) => console.debug(event));
+  return client;
 };
